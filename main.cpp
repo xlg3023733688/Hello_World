3c200b307c6bf33260c1be11335766922f0ceba9
xlg-shenyouchu <2130439131@qq.com>
2026-07-10 22:11:53 +0800

    优化冒泡排序

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void sort(int* arr,int size)
 {
     int tmp=0;
+    bool flag=false;
     for(int i = 1;i<size;i++)
     {
         for(int j = 1;j<=size-i;j++)
@@ -14,8 +15,10 @@ void sort(int* arr,int size)
                 tmp=arr[j-1];
                 arr[j-1]=arr[j];
                 arr[j]=tmp;
+                flag=true;
             }
         }
+        if(!flag) break;
     }
     for(int i = 0;i<size;i++) cout<<arr[i]<<" ";
     cout<<endl;
