daee1a608d2474480f0c050aa7bc9e8266dcda9c
xlg-shenyouchu <2130439131@qq.com>
2026-07-10 20:58:46 +0800

    实现sort

--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 void sort(int* arr,int size)
 {
-
+    int tmp=0;
+    for(int i = 1;i<size;i++)
+    {
+        for(int j = 1;j<=size-i;j++)
+        {
+            if(arr[j-1]>arr[j])
+            {
+                tmp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=tmp;
+            }
+        }
+    }
+    for(int i = 0;i<size;i++) cout<<arr[i]<<" ";
+    cout<<endl;
 }
 
 int main()
